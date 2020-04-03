69f281352f262bd1effa5cd8e374d342e551cf0d
Craig Thomas <craigt672@gmail.com>
2020-04-02 23:56:05 -0400

    [CT] add article title

--- functions/src/article-scrapper/foxs.ts
+++ functions/src/article-scrapper/foxs.ts
@@ -2,23 +2,27 @@ import * as puppeteer from 'puppeteer';
 
 import truncate from '../truncate';
 
-async function foxScrapper(url: string) {
+import { IArticle } from '../types';
+
+async function foxScrapper(url: string): Promise<IArticle> {
   const browser = await puppeteer.launch({headless: true});
   const page = await browser.newPage();
 
   await page.goto(url);
+
+  const title = await page.$eval('h1',  title => title.textContent);
 
   const paragraphs = await page.$$eval('.article-body > p', 
     p => p.map(p => p.innerHTML).filter(p => !p.startsWith('<strong>')&&!p.startsWith('<a'))
   );
 
   const sanitzedFirstBodyParagrah = paragraphs[0].replace(/<[^>]*>?/gm, '');
-
   const description = truncate(sanitzedFirstBodyParagrah, 40);
 
   await browser.close();
 
   return {
+    title,
     description,
     bodyContent: paragraphs
   }
