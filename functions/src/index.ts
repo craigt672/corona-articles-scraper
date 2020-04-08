489d31dedbc1bdd6bf18fe87c246585f584feffd
Craig Thomas <craigt672@gmail.com>
2020-04-08 18:39:14 -0400

    get articles from various sources

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,32 @@
 import * as functions from 'firebase-functions';
-import { fox } from './article-scrapper';
+import * as NewsAPI from 'newsapi';
 
+// import { fox } from './article-scrapper';
+
+const NEWS_TOPIC = 'coronavirus';
+const NEWS_API_KEY = '52339108ea7c4ba785ab195d44952c8e';
+
+const newsapi = new NewsAPI(NEWS_API_KEY);
+
+async function getArticles(): Promise<any[]> {
+  const { articles } =  await newsapi.v2.topHeadlines({
+    sources: 'fox-news,nbc-news,cbs-news,abc-news',
+    q: NEWS_TOPIC,
+    language: 'en',
+  });
+
+  return articles;
+}
 
 export const helloWorld = functions.https.onRequest(async (request, response) => {
-  const url = 'https://www.foxnews.com/media/coronavirus-questions-answered-loss-taste-smell-symptoms';
-  const articleData: any = await fox(url);
+  const articleData = await getArticles();
+
+  // const filteredFoxArticles = articleData.filter(article => {
+  //   return !article.url.startsWith('https://video');
+  // });
+
+  // const promisedFoxArticles = filteredFoxArticles.map(article => fox(article.url));
+  // const processedFoxArticles = await Promise.all(promisedFoxArticles);
+
   response.send(articleData);
 });
